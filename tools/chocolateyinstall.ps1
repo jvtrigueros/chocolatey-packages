034f0bfe602425ccf62d8e42aa094b0a83bd5af6
Jose V. Trigueros <j.v.trigueros@gmail.com>
2016-04-25 02:07:49 -0600

    do not create shim for executable

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,10 +1,11 @@
 $ErrorActionPreference = 'Stop';
 
+$exe          = 'Soundnode.exe'
 $iconUrl      = 'https://cdn.rawgit.com/Soundnode/soundnode-app/master/app/soundnode.ico'
 $packageName  = 'soundnode-app'
 $shortcutPath = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Soundnode App.lnk"
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$target       = Join-Path $toolsDir "Soundnode.exe"
+$target       = Join-Path $toolsDir $exe
 $url          = 'http://www.soundnodeapp.com/downloads/win/Soundnode-App.zip'
 
 $packageArgs = @{
@@ -19,6 +20,9 @@ $packageArgs = @{
 
 # Install from zip file
 Install-ChocolateyZipPackage @packageArgs
+
+# Don't create shim
+New-Item "$target.ignore" -type file -force | Out-Null
 
 # Get icon from repository
 Get-ChocolateyWebFile -packgeName $packageName -fileFullPath "$toolsDir\soundnode.ico" -url $iconUrl
