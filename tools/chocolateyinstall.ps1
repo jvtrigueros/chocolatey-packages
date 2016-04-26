b20563ebff553b96493536a3f8f14833ced72339
Jose V. Trigueros <j.v.trigueros@gmail.com>
2016-04-25 21:15:08 -0600

    improve installer by adding more files to the zip log

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -7,6 +7,8 @@ $shortcutPath = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Soundnode 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $target       = Join-Path $toolsDir $exe
 $url          = 'http://www.soundnodeapp.com/downloads/win/Soundnode-App.zip'
+$zipFileList  =  New-Object System.Text.StringBuilder
+$zipLogPath   = Join-Path $env:ChocolateyPackageFolder "${packageName}Install.zip.txt"
 
 $packageArgs = @{
   packageName   = $packageName
@@ -23,9 +25,14 @@ Install-ChocolateyZipPackage @packageArgs
 
 # Don't create shim
 New-Item "$target.ignore" -type file -force | Out-Null
+$zipFileList.AppendLine("$target.ignore") | Out-Null
 
 # Get icon from repository
 Get-ChocolateyWebFile -packgeName $packageName -fileFullPath "$toolsDir\soundnode.ico" -url $iconUrl
+$zipFileList.AppendLine("$toolsDir\soundnode.ico") | Out-Null
 
 # Create shortcut in Programs Start Menu
 Install-ChocolateyShortcut -shortcutFilePath $shortcutPath -targetPath "$target" -workingDirectory "$toolsDir" -iconLocation "$toolsDir\soundnode.ico" -description "Soundcloud desktop client"
+$zipFileList.Append("$shortcutPath") | Out-Null
+
+Add-Content $zipLogPath $zipFileList.ToString()
